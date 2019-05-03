733a5d0db956043d2018a02f72637ea2c9bebc86
Todd Sayre <captain.friendly@gmail.com>
2019-05-03 15:54:54 -0400

    Add custom image size size_525
    Added new image size for aesthetics of the new site news feeds

--- therex-child/functions.php
+++ therex-child/functions.php
@@ -1,3 +1,15 @@
 <?php
-// Put your custom code here
-?>
+/**
+ * PHP version 7.2
+ *
+ * The REX child theme for the Mercer News site
+ *
+ * @category TheREXChildTheme
+ * @package  TheREX
+ * @author   Todd Sayre <captain.friendly+doccomment@gmail.com>
+ * @license  ISC <https://opensource.org/licenses/ISC>
+ * @link     https://github.com/merceruniversity/wp-news/
+ */
+
+// Added for new website news feed appearances
+add_image_size('size_525', 525, 355, true);
